0776d2499e812d38d1da4871f08ece38ac906f1e
Noa <noamoshkovitz@post.runi.ac.il>
2025-11-19 21:08:25 +0200

    try

--- Algebra.java
+++ Algebra.java
@@ -26,42 +26,141 @@ public class Algebra {
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
 		// Replace the following statement with your code
-		return 0;
+		int res =  x1 ; 
+		for(int i=0 ; i<x2 ; i++){
+			res++;
+		}
+		return res;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
 		// Replace the following statement with your code
-		return 0;
+		int res = x1 ;
+		for(int i=0; i<x2 ; i++){
+			res--;
+		}
+		return res;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
+		if(x1 == 0 || x2==0){
+			return 0;
+		}
+		int res = 0;
+		for(int i=0;i<x2;i++){
+			for(int j=0; j<x1; j++){
+				res++;
+			}
+		}
 		// Replace the following statement with your code
-		return 0;
+		return res;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
+		if(n==0){
+			return 1;
+		}
+		if(n==1){
+			return x;
+		}
+		 
+		int result   = x; 
+		int next_power;
+		for(int i=0;i<n-1; i++){
+			next_power = 0;
+			for(int j=0; j < result ; j++){
+				for(int k=0 ; k< x ;k++){
+					
+					next_power++;
+				
+				}
+			}
+			//System.out.println("iteration n=" + (i+2) +"result="+temp_product);
+			result = next_power;
+		}
 		// Replace the following statement with your code
-		return 0;
+		return result;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
+		if(x2 == 0){
+			return -1; 
+		}
+		if(x2 == 1){
+			return x1;
+		}
+		int div_result = 1;
+		int temp = x1;
+		boolean flag = true;
+		while(flag){
+			for(int i=0 ; i<x2 ; i++){
+				temp--;
+			
+			}
+			if(temp >= x2){
+				div_result++;
+			}
+			else{
+				flag = false;
+			}
+		}
 		// Replace the following statement with your code
-		return 0;
+		return div_result;
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
 		// Replace the following statement with your code
-		return 0;
+		if(x2 == 0){
+			return -1; 
+		}
+		if(x2 == 1){
+			return 0;
+		}
+		if(x1<x2){
+			return x1;
+		}
+		int temp = x1;
+		boolean flag = true;
+		while(flag){
+			for(int i=0 ; i<x2 ; i++){
+				temp--;
+			
+			}
+			if(temp == x2){
+				return 0;
+		
+			}
+			if(temp<x2){
+				flag = false;
+
+			}
+		}
+		return temp;
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
+		int previous = 1 ;
+		for(int i=2 ; ;i++){
+			int current = pow(i,2);
+			if(x > previous && x < current){
+				return i-1;
+			}
+			if(x==current){
+				return i;
+			}
+			else{
+				previous = current ;
+			}
+			
+		}
+		
 		// Replace the following statement with your code
-		return 0;
+		
 	}	  	  
 }
--- Anagram.java
+++ Anagram.java
@@ -2,10 +2,10 @@
 public class Anagram {
 	public static void main(String args[]) {
 		// Tests the isAnagram function.
-		System.out.println(isAnagram("silent","listen"));  // true
-		System.out.println(isAnagram("William Shakespeare","I am a weakish speller")); // true
-		System.out.println(isAnagram("Madam Curie","Radium came")); // true
-		System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
+		//System.out.println(isAnagram("silent","listen"));  // true
+		//System.out.println(isAnagram("William Shakespeare","I am a weakish speller")); // true
+		//System.out.println(isAnagram("Madam Curie","Radium came")); // true
+		//System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
 
 		// Tests the preProcess function.
 		System.out.println(preProcess("What? No way!!!"));
@@ -14,7 +14,7 @@ public class Anagram {
 		System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
 		
 		// Performs a stress test of randomAnagram 
-		String str = "1234567";
+		String str = "silent";
 		Boolean pass = true;
 		//// 10 can be changed to much larger values, like 1000
 		for (int i = 0; i < 10; i++) {
@@ -28,8 +28,28 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
+		if(str1.length()!=str2.length()){
+			return false; 
+		}
+		boolean are_equal = true;
+		int len = str1.length();
+		boolean[] check = new boolean[str2.length()];
+		for(int i=0 ; i<len ; i++){
+			for(int j=0 ; j<len ; j++){
+				if(str1.charAt(i) == str2.charAt(j) && check[j] == false){
+					check[j] = true ;
+					break;
+
+				}
+				else if (j == len-1 ){
+					are_equal = false;
+					break;
+				}
+
+			}
+		}
 		// Replace the following statement with your code
-		return false;
+		return are_equal;
 	}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
@@ -37,13 +57,42 @@ public class Anagram {
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
 		// Replace the following statement with your code
-		return "";
+		String new_str ="";
+		for(int i=0 ; i<str.length(); i++){
+			// check if character is an upper case letter, and if so we transform it to new_str
+			if(((str.charAt(i) >= 65 && str.charAt(i)<=90))){
+				new_str = new_str + (char)(str.charAt(i)+32);
+			}
+			// check if character is lower case letter or space, if so we add it to new_str 
+			else if (str.charAt(i)>=97 && str.charAt(i)<=122||str.charAt(i) == 32){
+				new_str = new_str + (char)(str.charAt(i));
+			}
+		
+		}
+		return new_str;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
-	// characters as the given string, re-arranged in a random order. 
+	// characters as the given string, re-asrranged in a random order. 
 	public static String randomAnagram(String str) {
 		// Replace the following statement with your code
-		return "";
+		
+		int strlen = str.length();
+		int j;
+		char[] char_arr = str.toCharArray();
+		char temp = ' ';
+		for ( int i = strlen-1  ; i>0 ; i-- ){
+			j = (int) (Math.random() * (double) (i+1));
+			temp        = char_arr[i];
+			char_arr[i] = char_arr[j];
+			char_arr[j] = temp ;
+
+		}
+		String my_str ="";
+		for(int i=0 ;  i<strlen; i++){
+			my_str = my_str + char_arr[i];
+
+		}
+		return my_str;
 	}
 }
